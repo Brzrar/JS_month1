85eb11caeb987b24762d5e9d42a941f4cdac96b9
Basirahamnov63 <113968574+Brzrar@users.noreply.github.com>
2023-04-19 20:08:02 +0600

    hw1
    hw1

--- home_W1/app.js
+++ home_W1/app.js
@@ -1,22 +1,22 @@
 // hw 1
 
 // 1) Приветствие
-// var name = prompt("Имя:")
-// var surname = prompt("Фамилия")
-// alert("Здравствуйте, " + name + " " + surname + "!")
+var name = prompt("Имя:")
+var surname = prompt("Фамилия")
+alert("Здравствуйте, " + name + " " + surname + "!")
 
 // 2) Числа, >, <, =
 
-// var num1 = prompt("Введите первое число: ")
-// var num2 = prompt("Введите второе число: ")
-//
-// if (num1 > num2) {
-//     console.log(num1 + " Больше " + num2)
-// } else if (num1 < num2) {
-//     console.log(num1 + " Меньше " + num2)
-// } else {
-//     console.log(num1 + " равно " + num2)
-// }
+var num1 = prompt("Введите первое число: ")
+var num2 = prompt("Введите второе число: ")
+
+if (num1 > num2) {
+    console.log(num1 + " Больше " + num2)
+} else if (num1 < num2) {
+    console.log(num1 + " Меньше " + num2)
+} else {
+    console.log(num1 + " равно " + num2)
+}
 
 // 3) Светофор
 
